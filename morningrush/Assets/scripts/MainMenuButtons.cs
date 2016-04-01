33c820f2457b334d7e6df6c1237af6a2a31e4f19
akoumandarakis <kalphabet111@gmail.com>
2016-03-31 18:07:28 -0600

    Main Menu
    I added a main menu scene with four buttons: Start, How to Play,
    Credits, and To Website. I created two empty scenes for the credits and
    tutorial, the start button takes the player to level1, and To Website
    opens the starbucks website in browser.

---
+++ morningrush/Assets/scripts/MainMenuButtons.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void startButtonPress()
+    {
+        Application.LoadLevel("level1");
+    }
+
+    public void creditsButtonPress()
+    {
+        Application.LoadLevel("credits");
+    }
+
+    public void howToButtonPress()
+    {
+        Application.LoadLevel("tutorial");
+    }
+
+    public void toWebsiteButtonPress()
+    {
+        Application.OpenURL("http://www.starbucks.com/");
+    }
+}
